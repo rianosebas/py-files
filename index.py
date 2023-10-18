97a720daaab5a4a3228588416782c7b66646de48
robinparadise <robinchogiles@gmail.com>
2023-10-18 12:40:39 +0200

    exercise

---
+++ index.py
@@ -0,0 +1,17 @@
+# import your functions here
+
+# read the quijote here
+
+
+# Word Count
+# print('Word Count: ', files.wordCount(book))
+
+# Unique Word Count
+# print('Unique Word Count: ', files.uniqueWordCount(book))
+
+# Quijote count
+# print('find Content: ', files.findContent(book, 'quijote'))
+# print('find Content: ', files.findContent(book, 'sancho'))
+
+# Change Quijote to Quixote and write it to a new file "el_quixote.txt"
+# print('Change Quijote to Quixote: ', files.changeQuijoteToQuixote(book))
---
+++ utils/files.py
@@ -0,0 +1,15 @@
+def readFile(name):
+  # return file
+  return ""
+
+def wordCount(text):
+  # return count
+  return 0
+
+def uniqueWordCount(text):
+  # return count
+  return 0
+
+def findContent(text, word):
+  # return count
+  return 0
